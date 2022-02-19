5ffcb232a700e20fbfc3cea034599eb6f54fa9ac
King <70107167+Programador-jr@users.noreply.github.com>
2022-02-19 06:51:12 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initia-scale=1">
+    <title>Gerador de Curriculo</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+  <body>
+		<nav class="navbar navbar-dark bg-dark">
+		  <div class="container-fluid">
+		    <a class="navbar-brand" href="#">Gerador de Curriculo</a>
+		  </div>
+		</nav>
+		<main class="container">
+			<form method="post" target="_blank" enctype="multipart/form-data">
+				<div class="card">
+					<div class="card-body">
+						<h4 class="card-title">Informações Gerais</h4>
+					</div>
+				</div>
+			</form>
+		</main>
+  </body>
+</html>
